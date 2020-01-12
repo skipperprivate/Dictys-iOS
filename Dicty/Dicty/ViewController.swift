1f0a32f618e005e8d39d8de34c68f7d251832aa6
Egor Bedov <bedovegor2203@gmail.com>
2020-01-12 20:23:03 +0300

    init commit

---
+++ Dicty/Dicty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicty
+//
+//  Created by Егор Бедов on 12/01/2020.
+//  Copyright © 2020 Егор Бедов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dicty/DictyTests/DictyTests.swift
@@ -0,0 +1,34 @@
+//
+//  DictyTests.swift
+//  DictyTests
+//
+//  Created by Егор Бедов on 12/01/2020.
+//  Copyright © 2020 Егор Бедов. All rights reserved.
+//
+
+import XCTest
+@testable import Dicty
+
+class DictyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
